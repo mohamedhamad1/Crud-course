08446d20a16d450d35e1318c499f88fb924b5da4
NfSwilam01 <66533353+mohamedhamad1@users.noreply.github.com>
2024-02-14 02:01:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const coursesRouters = require('./routes/courses-routes');
+const usersRouters = require('./routes/users-routes');
+const httpStatusText = require('./utils/httpStatusText')
+const mongoose = require('mongoose');
+const port = process.env.PORT;
+const app = express();
+const path = require('path');
+
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
+
+mongoose.connect('mongodb://127.0.0.1:27017/crud').then(() => {
+    console.log('Connected to MongoDB');
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/courses', coursesRouters)
+app.use('/api/users', usersRouters)
+
+app.all('*', (req, res, next) => {
+    return res.json(404, { status: httpStatusText.ERROR, message: 'this resource is not available' })
+})
+
+app.use((error, req, res, next) => {
+    res.json({status: error.statusText||httpStatusText.ERROR, msg:error.message,code:error.statusCode || 500,data:null})
+});
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+})
