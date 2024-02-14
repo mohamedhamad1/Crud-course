fcd481209c34a95b0c2d4a6c8a8f38d8f3580657
NfSwilam01 <66533353+mohamedhamad1@users.noreply.github.com>
2024-02-14 22:02:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const mongoose = require('mongoose');
 const port = process.env.PORT;
 const app = express();
 const path = require('path');
-
+const uri = process.env.MONGODB_URI;
 app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
-mongoose.connect('mongodb://127.0.0.1:27017/crud').then(() => {
+mongoose.connect(uri).then(() => {
     console.log('Connected to MongoDB');
 })
 
